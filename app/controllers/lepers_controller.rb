0dee863fbec728520c417ab1ddfe7c5b56ca922d
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-09 17:08:12 +0200

    longest word game

--- app/controllers/lepers_controller.rb
+++ app/controllers/lepers_controller.rb
@@ -1,7 +1,51 @@
+require 'open-uri'
+require 'json'
+
 class LepersController < ApplicationController
   def game
+    @grid = Array.new(9) { ('A'..'Z').to_a[rand(26)] }
   end
 
   def score
+    @attempt = params[:attempt]
+    @grid = params[:grid]
+    @start_time = Time.parse(params[:start_time])
+    @end_time = Time.now
+    @result = { time: @end_time - @start_time }
+    @result[:translation] = get_translation(@attempt)
+    @result[:score], @result[:message] = score_and_message(
+    @attempt, @result[:translation], @grid, @result[:time])
   end
+
+  def get_translation(word)
+    response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word.downcase}")
+    json = JSON.parse(response.read.to_s)
+    json['term0']['PrincipalTranslations']['0']['FirstTranslation']['term'] unless json["Error"]
+  end
+
+  def score_and_message(attempt, translation, grid, time)
+    if translation
+      if included?(@attempt.upcase, grid)
+        score = compute_score(@attempt, time)
+        [score, "well done"]
+      else
+        [0, "not in the grid"]
+      end
+    else
+      [0, "not an english word"]
+    end
+  end
+
+  def included?(guess, grid)
+    the_grid = grid.clone.split(" ")
+    guess.chars.each do |letter|
+      the_grid.delete_at(the_grid.index(letter)) if the_grid.include?(letter.upcase)
+    end
+    grid.gsub(" ", "").size == guess.size + the_grid.size
+  end
+
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'lepers/game'
+  get 'game' => 'lepers#game'
 
-  get 'lepers/score'
+  get 'score' => 'lepers#score'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
